8d00d116314a29df3d9a6a48810a1b2bd658a095
Revanth47 <7revanth47@gmail.com>
2016-07-08 11:50:07 +0530

    parse CLI arguements
    Parse the CLI args and use it to start the subprocess(app to watch)

--- gowatch.go
+++ gowatch.go
@@ -2,41 +2,57 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"time"
 
 	"gopkg.in/fsnotify.v1"
 )
 
 const (
-	threshold = 100*time.Millisecond
+	threshold = 100 * time.Millisecond
 )
 
 func main() {
-	watcher,err := fsnotify.NewWatcher()
+	args := os.Args[1:]
+	cmd := exec.Command("go", args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Starting App")
+
+	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer watcher.Close()
+
 	done := make(chan bool)
 	go func() {
 		t := time.Now()
 		for {
 			select {
 			case event := <-watcher.Events:
-				if(time.Since(t)>=threshold) {
+				if time.Since(t) >= threshold && filepath.Ext(event.Name) == ".go" || filepath.Ext(event.Name) == ".tmpl" {
 					t = time.Now()
-					log.Println("event",event)
+					log.Println("event", event)
 					if event.Op&fsnotify.Write == fsnotify.Write {
 						log.Println("modified file:", event.Name)
 					}
 				}
 			case err := <-watcher.Errors:
-				log.Println("error:",err) 	
+				log.Println("error:", err)
 			}
 		}
 	}()
 
 	err = watcher.Add(".")
+
 	if err != nil {
 		log.Fatal(err)
 	}
